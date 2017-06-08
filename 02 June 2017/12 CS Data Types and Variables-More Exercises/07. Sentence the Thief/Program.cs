8bb048c4b37d1935a440a0dd6c0d1ac1b2b9c12c
AngryBaboon <IvayloCoding@gmail.com>
2017-06-08 13:36:36 +0300

    3

---
+++ 02 June 2017/12 CS Data Types and Variables-More Exercises/07. Sentence the Thief/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Sentence_the_Thief
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numType = Console.ReadLine();
+            var n = int.Parse(Console.ReadLine());
+            long id = long.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var tempId = long.Parse(Console.ReadLine());
+
+                switch (numType)
+                {
+                    case "sbyte":
+                        if (tempId >= sbyte.MinValue && tempId <= sbyte.MaxValue)
+                        {
+                            if (id < tempId)
+                                id = tempId;
+                        }
+                        break;
+                    case "int":
+                        if (tempId >= int.MinValue && tempId <= int.MaxValue)
+                        {
+                            if (id < tempId)
+                                id = tempId;
+                        }
+                        break;
+                    case "long":
+                        if (tempId >= long.MinValue && tempId <= long.MaxValue)
+                        {
+                            if (id < tempId)
+                                id = tempId;
+                        }
+                        break;
+                    default: break;
+                }
+            }
+
+            double sentence = 0;
+
+            if (id > 0)
+                sentence = Math.Ceiling((double)id / sbyte.MaxValue);
+            else
+                sentence = Math.Ceiling((double)id / sbyte.MinValue);
+
+            Console.WriteLine("Prisoner with id {0} is sentenced to {1} {2}", id, sentence, sentence > 1 ? "years" : "year");
+        }
+    }
+}
---
+++ 02 June 2017/12 CS Data Types and Variables-More Exercises/08. House Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.House_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = long.Parse(Console.ReadLine());
+            var n2 = long.Parse(Console.ReadLine());
+
+            long totalPrice = 0;
+
+            if (n1 <= sbyte.MaxValue && n2 > sbyte.MaxValue)
+                totalPrice = (n1 * 4) + (n2 * 10);
+            else
+                totalPrice = (n2 * 4) + (n1 * 10);
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
